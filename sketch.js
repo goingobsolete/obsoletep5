e0a9884524e1d8dd49721e119718e451e2249b79
goingobsolete <goingobsolete@gmail.com>
2024-06-06 14:46:11 +0200

    set brush to 100 opacity and added randomness to brushsizeX also no more center text align to text

--- sketch.js
+++ sketch.js
@@ -1,7 +1,36 @@
+let brushSizeX = 10;
+let brushSizeY = 30
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  background(255);
+
 }
 
 function draw() {
-  background(220);
+  // Set the brush color to black
+  fill(random(255),random(255),random(255));
+  noStroke();
+  brushSizeX = 10+random(20);
+  // brushSizeY = 10+random(20);
+  // brushSizeX = 20;
+  // brushSizeY = 50;
+  
+  //ideally this would be in proportion to window size like all variables
+
+  
+  // Draw a circle at the mouse position to reveal the text
+  if (mouseIsPressed) {
+    ellipse(mouseX, mouseY, brushSizeX, brushSizeX*3);
+      if (frameCount % 4 < 1) {
+        push();
+        textSize(random(200));
+        textFont('Helvetica');
+        // textAlign(CENTER, CENTER);
+        stroke(0);
+        strokeWeight(3);
+        fill(255);
+        text("obsolete", random(width), random(height));
+        pop();
+    }
+  }
 }
