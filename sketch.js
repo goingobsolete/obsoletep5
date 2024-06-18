9a995c79362c2bd8c608abe6ac6140293a7e93a6
goingobsolete <goingobsolete@gmail.com>
2024-06-18 15:00:19 +0200

    changed text brush

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-let myFont;
+// let myFont;
 
 let brushSizeX = 10;
 let brushSizeY = 30
@@ -7,9 +7,9 @@ let noiseOffsetR = 0;
 let noiseOffsetG = 10000; // Start at different offsets for each color
 let noiseOffsetB = 20000; // to ensure they vary independently
 
-function preload() {
-  myFont = loadFont('assets/IBMPlexSans-Bold.ttf');
-}
+// function preload() {
+  // myFont = loadFont('assets/IBMPlexSans-Bold.ttf');
+// }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -27,15 +27,15 @@ function draw() {
   // Set the brush to noisy color
   fill(255,g,b);
   noStroke();
-  brushSizeX = 30+random(20);
+  brushSizeX = ((width*0.05)+random(20));
   // brushSizeY = 10+random(20);
   // brushSizeX = 20;
   // brushSizeY = 50;
 
   // Increment noise offsets for continuous color change
-  noiseOffsetR += 0.05;
-  noiseOffsetG += 0.05;
-  noiseOffsetB += 0.05;
+  noiseOffsetR += 0.1;
+  noiseOffsetG += 0.1;
+  noiseOffsetB += 0.01;
   
   //ideally this would be in proportion to window size like all variables
 
@@ -43,20 +43,22 @@ function draw() {
   // Draw a circle at the mouse position to reveal the text
   if (mouseIsPressed) {
     push();
-        textSize(100+random(100));
-        textFont(myFont);
+        // ellipse(mouseX, mouseY, brushSizeX*6, brushSizeX*3);
+        textSize(150+random(5));
+        textFont('HELVETICA');
         textAlign(CENTER, CENTER);
+        noStroke
         stroke(255);
-        strokeWeight(3);
+        strokeWeight(1);
         fill(random(255),random(255),random(255));
-        // fill(r,g,b);
-        text("going", mouseX, mouseY);
+        fill(r,g,b);
+        text("GOING", mouseX, mouseY);
         pop();
     // ellipse(mouseX, mouseY, brushSizeX, brushSizeX*3);
     // ellipse(mouseX+brushSizeX, mouseY, brushSizeX, brushSizeX*3);
       // if (frameCount % 4 < 1) {
         push();
-        textSize(random(200));
+        textSize(random(150));
         textFont('Helvetica');
         textAlign(CENTER, CENTER);
         stroke(0);
