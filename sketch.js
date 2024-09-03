942f5c6dfc3a20dc18ed041a4fb8b842e207e4ef
goingobsolete <goingobsolete@gmail.com>
2024-09-03 11:42:15 +0200

    added color, and size variables

--- sketch.js
+++ sketch.js
@@ -1,70 +1,61 @@
-let myFont;
+let goFont;
+let goFill;
+let goSize;
+let goRatio;
 
-let brushSizeX = 10;
-let brushSizeY = 30
+let obFont;
 
-let noiseOffsetR = 0;
-let noiseOffsetG = 10000; // Start at different offsets for each color
-let noiseOffsetB = 20000; // to ensure they vary independently
+
+
 
 function preload() {
-  // myFont = loadFont('assets/IBMPlexSans-Bold.ttf');
-  myFont = 'HELVETICA'
+  // goFont = loadFont('assets/IBMPlexSans-Bold.ttf');
+  goFont = 'HELVETICA'
+  obFont = goFont
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(255);
-
+  background(0);
 }
 
 function draw() {
 
-  // Generate color values using Perlin noise
-  let r = random(255);
-  let g = random(255);
-  let b = random(255);
-
-  // Set the brush to noisy color
-  fill(r,g,b);
-  noStroke();
-  brushSizeX = ((width*0.05)+random(20));
-  // brushSizeY = 10+random(20);
-  // brushSizeX = 20;
-  // brushSizeY = 50;
-
-  // Increment noise offsets for continuous color change
-  noiseOffsetR += 0.1;
-  noiseOffsetG += 0.1;
-  noiseOffsetB += 0.01;
+  // Going Object Fill
+  goFill = {
+    r: random(255),
+    g: random(255),
+    b: random(255),
+    o: 255
+  };
   
   //ideally this would be in proportion to window size like all variables
 
-  
-  // Draw a circle at the mouse position to reveal the text
   if (mouseIsPressed) {
-    push();
-        // ellipse(mouseX, mouseY, brushSizeX*6, brushSizeX*3);
-        textSize(150+random(20));
-        textFont(myFont);
+        push();
+        textSize(300);
+        textFont(goFont);
         textAlign(CENTER, CENTER);
-        noStroke
         stroke(255);
-        strokeWeight(1);
-        fill(random(255),random(255),random(255));
-        fill(r,g,b);
-        text("GOING", mouseX, mouseY);
+        strokeWeight(random(3));
+        fill(goFill.r, goFill.g, goFill.b, goFill.o);
+        //snag the colors from riso**** every lift switched colors
+        // fill(r,g,b);
+        
+
+        translate(mouseX, mouseY, 6);
+        // rotate(radians(frameCount*-1));
+        text("GOING", 0, 0,0);
         pop();
-    // ellipse(mouseX, mouseY, brushSizeX, brushSizeX*3);
-    // ellipse(mouseX+brushSizeX, mouseY, brushSizeX, brushSizeX*3);
+
       // if (frameCount % 4 < 1) {
         push();
         textSize(random(150));
-        textFont(myFont);
+        textFont(obFont);
         textAlign(CENTER, CENTER);
         stroke(0);
         strokeWeight(3);
-        fill(255);
+        fill(255,255,255,random(215,216));
         text("obsolete", random(width), random(height));
         pop();
     // }
